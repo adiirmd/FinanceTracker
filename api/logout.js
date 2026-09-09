b86301de77a8536a7d2b55b30e8ef14bb9185483
adiirmd <adiromadhon0@gmail.com>
2026-09-09 08:15:39 +0700

    Hapus endpoint logout dan me yang tidak lagi dipakai
    Keduanya sisa dari sesi berbasis cookie yang sudah diganti token di memori.

--- api/logout.js
+++
@@ -1,6 +0,0 @@
-const { clearSessionCookie } = require("../lib/auth");
-
-module.exports = async (req, res) => {
-  res.setHeader("Set-Cookie", clearSessionCookie());
-  return res.status(200).json({ ok: true });
-};
